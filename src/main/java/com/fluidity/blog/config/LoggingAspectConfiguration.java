a8ed4006fe42b2077f90287ed496af16adfc7766
maxproof <max.karberg@gmail.com>
2018-07-21 23:47:55 +0200

    Initial application generated by JHipster-5.1.0

---
+++ src/main/java/com/fluidity/blog/config/LoggingAspectConfiguration.java
@@ -0,0 +1,19 @@
+package com.fluidity.blog.config;
+
+import com.fluidity.blog.aop.logging.LoggingAspect;
+
+import io.github.jhipster.config.JHipsterConstants;
+
+import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class LoggingAspectConfiguration {
+
+    @Bean
+    @Profile(JHipsterConstants.SPRING_PROFILE_DEVELOPMENT)
+    public LoggingAspect loggingAspect(Environment env) {
+        return new LoggingAspect(env);
+    }
+}
---
+++ src/main/java/com/fluidity/blog/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.fluidity.blog.config.audit;
